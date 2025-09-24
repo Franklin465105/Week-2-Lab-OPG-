e914a0c5eee01e487aac98ad51ca680465ea4d69
Franklin Esedebe <G00465105@atu.ie>
2025-09-24 17:27:31 +0100

    Part 3 Answer

--- src/main/java/ie/atu/week2/Calculator.java
+++ src/main/java/ie/atu/week2/Calculator.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 
 
 public class Calculator {
+
+
     public static void main(String[] args) {
 
-        //Prompt the user for a second number
-        // Display the total to the screen
         Scanner scan1 = new Scanner(System.in);
+
         //Please first number
         System.out.println("Please enter a number: ");
         double firstNum = scan1.nextDouble();
@@ -22,18 +23,31 @@ public class Calculator {
         double secondNum = scan1.nextDouble();
         System.out.println("You entered " + secondNum);
 
-        double sum = firstNum + secondNum;
-        System.out.println("The sum is: " + sum);
+        System.out.println("Please enter an operation(add, subtract, multiply, divide): ");
+        String operation = scan1.next();
 
+        boolean validOperation = true;
+        double result = switch (operation) {
+            case "add" -> {
+                yield firstNum + secondNum;
+            }
+            case "subtract" -> {
+                yield firstNum - secondNum;
+            }
+            case "multiply" -> {
+                yield firstNum * secondNum;
+            }
+            case "divide" -> {
+                yield firstNum / secondNum;
+            }
 
+            default -> {
+                System.out.println("Invalid operation");
+                yield 0;
+            }
 
-
-
-
-
-
-
+        };
+        System.out.println("The answer is " + result);
     }
-
 
 }
